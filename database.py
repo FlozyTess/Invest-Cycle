4f6a9d7771ba4c1ed8f0004188ce8dcf7c4088f3
FlozyTess <cecilrency23@gmail.com>
2025-03-10 09:13:26 +0300

    updated

--- database.py
+++ database.py
@@ -1,2 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+
+DATABASE_URL = "sqlite:///investcycle.db"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine)
+Base = declarative_base()
