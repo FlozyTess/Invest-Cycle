5af222097c1ed1a3daf9cd7f4193937f07f2f741
FlozyTess <cecilrency23@gmail.com>
2025-03-11 23:51:55 +0300

    created sessions and tables in db

--- db/database.py
+++ db/database.py
@@ -4,3 +4,9 @@ from models import Base
 
 # Create database connection
 engine = create_engine('sqlite:///db/investcycle.db')
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# Create tables
+Base.metadata.create_all(engine)
