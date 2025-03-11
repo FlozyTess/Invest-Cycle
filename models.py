9e384f12df69e868f01caeb7455a6fd2659f117a
FlozyTess <cecilrency23@gmail.com>
2025-03-11 23:36:27 +0300

    created and defined class user

--- models.py
+++ models.py
@@ -4,3 +4,11 @@ from sqlalchemy.orm import relationship
 import datetime
 
 Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    balance = Column(Float, default=0.0)
