583cb8355ade70494dae89294c138d4c971ec6fe
FlozyTess <cecilrency23@gmail.com>
2025-03-12 00:34:45 +0300

    added a function to check if user exists

--- seed.py
+++ seed.py
@@ -5,16 +5,24 @@ from models import User, Investment
 Session = sessionmaker(bind=engine)
 session = Session()
 
+#checks if user exists
+def user_exists(email):
+    return session.query(User).filter_by(email=email).first() is not None
+
 #Add users
-user1 = User(name="Joy",email="joy@outlook.com")
-user2 = User(name="Gabby",email="gabby@outlook.com")
+user1 = User(name="Joy",email="joy@outlook.com",balance=3000)
+user2 = User(name="Gabby",email="gabby@gmail.com",balance=5000.00)
 
 session.add_all([user1, user2])
 session.commit()
 
 #Add Investments
-investment1 = Investment(user_id=1, amount=2000.80, investment_type="Stock")
-investment2 = Investment(user_id=2, amount=1000.50, investment_type="Crypto")
+investment1 = Investment(user_id=1, amount=2000.80, name="Stock")
+investment2 = Investment(user_id=2, amount=1000.50, name="Crypto")
 
 session.add_all([investment1, investment2])
 session.commit()
+
+print("Sample data inserted successfully!")
+
+session.close()
