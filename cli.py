e70e7c2dbff58a833993889594cafd1b2602333f
FlozyTess <cecilrency23@gmail.com>
2025-03-13 14:24:51 +0300

    log in and password functions updated

--- cli.py
+++ cli.py
@@ -3,3 +3,17 @@ import getpass
 from sqlalchemy.orm import sessionmaker
 from db.database import engine
 from models import User
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+def hash_password(password):        # for hashing the password
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+def verify_password(stored_password, entered_password):         # to verify the password
+    return bcrypt.checkpw(entered_password.encode('utf-8'), stored_password) 
+
+def signup():           # for signing up users
+    name = input("Enter your name: ")
+    email = input("Enter your email: ")
+
