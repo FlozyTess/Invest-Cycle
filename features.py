4261869ca982fd7af8e0b0ed725290be01299039
FlozyTess <cecilrency23@gmail.com>
2025-03-13 19:32:28 +0300

    delete group

--- features.py
+++ features.py
@@ -26,5 +26,34 @@ def check_and_remove_defaulters(group_id):
         
         session.commit()
 
+        # To edit group details
+def edit_group(group_id):
+    group = session.query(Group).filter_by(id=group_id).first()
+    
+    if not group:
+        print(No group found.)
+        return
+
+    New_name = input(f"Enter new name (current: {group.name}): ")
+    new_amount = input(f"Enter new contribution amount (current: {group.contribution_amount}): ")
+
+    if new_name:
+        group.name = new_name
+    if new_amount:
+        group.contribution_amount = float(new_amount)
+
+    session.commit()
+        print("Group updated")
+
+
+        # deleting a group
+def delete_group(group_id):
+    group = session.query(Group).filter_by(id=group_id).first()
+
+    if not group:
+        print("group does not exist.")
+        return
+         
+
 
 
