aeb30a640527bdd3bbdf78d5cec42300e5c1181f
FlozyTess <cecilrency23@gmail.com>
2025-03-13 18:19:28 +0300

    updated features.py

--- features.py
+++ features.py
@@ -1,6 +1,6 @@
 import datetime
 from sqlalchemy.orm import sessionmaker
-from database import engine
+from db.database import engine
 from models import User, GroupMember, Contribution, Payout
 
 Session = sessionmaker(bind=engine)
