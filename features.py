5c5fec5da7141b4a7043e57a91202d777e121bea
FlozyTess <cecilrency23@gmail.com>
2025-03-13 18:09:50 +0300

    check for members

--- features.py
+++ features.py
@@ -5,3 +5,10 @@ from models import User, GroupMember, Contribution, Payout
 
 Session = sessionmaker(bind=engine)
 session = Session()
+
+        # check for missed contributions and defaulters
+def check_and_remove_defaulters(group_id):
+    members = session.query(GroupMember).filter_by(group_id=group_id, is_active=True).all()  
+
+    for member in members:
+        user = session.query(User).filter_by(id=member.user_id).first()  
